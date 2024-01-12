23000f9a6642ab8383313ba76fe10f535350221d
NightDev4l <122033083+shviam-2018@users.noreply.github.com>
2024-01-12 22:44:56 +0000

    speed builded in cpp and made a cpp speed build time logger and a practice folder with a practice.cpp that says ready for use start at your will

--- repetor/main.cpp
+++ repetor/main.cpp
@@ -6,42 +6,44 @@ void displayresult(const string& message, int times);
 
 int main(){
     string What_to_say;
-    int times;
+    int time;
 
     while (true)
     {
-        cout << "what do youo what me to say: ";
+        cout << "what do you want me to say: ";
         cin >> What_to_say;
-        cout << "you what to say: "<<What_to_say<<endl;
+        cout << "you what me to say: " << What_to_say << endl;
 
-        string confirm_WTS;
-        cout<<"is this correct(y/n): ";
-        cin >> confirm_WTS;
-        if(confirm_WTS == "y"){
+        string confirm_What_to_say;
+        cout << "is this correct (y/n): ",
+        cin>> confirm_What_to_say;
+        if(confirm_What_to_say == "y") {
             break;
         }
     }
 
     while (true)
     {
-        cout<<"how many time do you what me to say: "<<What_to_say<<endl;
-        cout<<"enter number of times: ";
-        cin >> times;
-        cout<<"you what me to say this: "<<times<<endl;
+        cout << "how many times you what me to say " << What_to_say << endl;
+        cout << "enter number of times: ";
+        cin >> time;
+        cout << "you what me to say: " << What_to_say << " " << time << " " << "times" << endl;
 
-        string confirm_T;
-        cout<<"is this correect(y/n): ";
-        cin>>confirm_T;
-        if(confirm_T=="y"){
+        string confirm_t;
+        cout << "is this correct (y/n): ";
+        cin >> confirm_t;
+        if (confirm_t == "y") {
             break;
         }
     }
-    displayresult(What_to_say, times);
-return 0;
+    
+    
+    displayresult(What_to_say, time);
+    return 0;
 }
 
 void displayresult(const string& message, int times) {
-        for( int i = 0; i<times; ++i) {
-            cout<<i<<" "<<message<<endl;
-        }
+    for(int i=0; i<times; ++i) {
+        cout << i+1 << " " << message << endl;
     }
+}
